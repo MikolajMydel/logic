c2865aec32c769fc782022aa2727c8ff4910073d
isayni <lennysep@protonmail.com>
2021-12-09 19:38:42 +0100

    React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+            Logic Circuit Simulator
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

5d8512db31d4a6a4b7d5d0e31794498381ab2c9b
MikolajMydel <76210468+MikolajMydel@users.noreply.github.com>
2021-12-14 10:10:57 +0100

    Update ControlPanel.js

--- src/Components/ControlPanel/ControlPanel.js
+++ src/Components/ControlPanel/ControlPanel.js
@@ -7,7 +7,7 @@ const ControlPanel = (props) => (
         <button onClick={ () => props.addElement ( { type: "logicGate", gateLogic: "AND", inputCount: 2 } ) } > Bramka AND </button>
         <button onClick={ () => props.addElement ( { type: "logicGate", gateLogic: "OR", inputCount: 2 } ) } > Bramka OR </button>
 
-        <button onClick={ () => props.addElement ( { type: "logicGate", gateLogic: "OR", inputCount: 5 } ) } > Bramka OR </button>
+        <button onClick={ () => props.addElement ( { type: "logicGate", gateLogic: "OR", inputCount: 5 } ) } > Bramka testowa (5 wejść) </button>
     </nav>
 )
 

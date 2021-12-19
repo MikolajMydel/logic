65c4efb0f721beefd8b4f6d41524e19382da0b9f
MikolajMydel <mydelmikolaj@gmail.com>
2021-12-19 09:57:51 +0100

    Inteligentne funkcje logiczne

--- src/Components/LogicGate/LogicGate.js
+++ src/Components/LogicGate/LogicGate.js
@@ -2,7 +2,7 @@ import React from "react";
 import Pin from "./Pin";
 import styles from "./LogicGate.module.scss";
 
-import {OR} from './LogicalFunctions.js';
+import {AND, OR} from './LogicalFunctions.js';
 
 const gateClass = {
     'AND': styles.LogicGateAND,
@@ -10,7 +10,7 @@ const gateClass = {
     'NOT': styles.LogicGateNOT,
 }
 const basicFunctions = {
-    'AND': (i) => (i[0] && i[1]),
+    'AND': (i) => AND(i),
     'OR':  (i) => OR(i),
     'NOT': (i) => !(i[0]),
 }
@@ -34,27 +34,19 @@ class LogicGate extends React.Component {
         }
     }
     processOutput = () => {
-        let inputs = [];
-        for (let i = 0; i < this.inputs.length; i++){
-            let inp = this.inputs[i].state.value; // true or false
 
-            /* 
+        /* 
                 nawet jezeli brakuje ktoregos inputa, to w przypadku bramek AND i OR mozna okreslic wyjscie na podstawie
                 jednej wartosci (np. AND na pewno bedzie falszywe jezeli jedno wejscie jest falszywe lub OR na pewno jest
                 prawdziwe jezeli chociaz jedna wartosc jest prawdziwa )
 
-                Dzieki temu bedzie mozna robic bramki majace swoje wyjscie na wejsciu
-            */
+                Dzieki temu bedzie mozna robic uklady zapamietujace stan
+        */
 
-            inputs.push(inp);
-            /*
-            if (inputs[i] === undefined){
-                for (let j = 0; j < this.outputs.length; j++)
-                    this.outputs[j].receiveSignal(undefined);
-                return;
-            }
-            */
-        }
+        let inputs = Array.from(
+            this.inputs.map ( (input) => input.state.value )
+        );
+        
         let output = this.func(inputs);
         // na razie używamy tylko bramek z jednym outputem więc whatever
         this.outputs[0].receiveSignal(output);
--- src/Components/LogicGate/LogicalFunctions.js
+++ src/Components/LogicGate/LogicalFunctions.js
@@ -1,11 +1,26 @@
 export function OR ( inputs ){
-
-    console.log ( inputs );
-
     for (let i = 0; i < inputs.length; i++){
         if ( inputs[i] === true) return true;
     }
-
-    // nie ma ani jednej wartosci true
     return false;
+}
+
+// jezeli jest chociaz jeden false - zwroc false
+// jezeli jest chociaz jeden undefined - zwroc undefined
+// sprawdzam czy byl undefined dopiero na koncu funkcji
+// aby miec pewnosc, ze nie ma na nastepnych miejscach
+// false (poniewaz wowczas mamy pewnosc, ze wartoscia bramki
+// jest falsz)
+export function AND ( inputs ){
+
+    let isUndefined = false;
+
+    for (let i = 0; i < inputs.length; i++){
+        if (inputs[i] === false) return false;
+        if (inputs[i] === undefined) isUndefined = true;
+    }
+
+    if ( isUndefined ) return undefined;
+    else return true;
+
 }

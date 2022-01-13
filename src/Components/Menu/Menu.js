41bb2a756ef3523bdeb513fda139d73c7aa968d3
isayni <lennysep@protonmail.com>
2022-01-13 16:56:24 +0100

    jako div

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,11 +3,13 @@ import Button from './Button'
 
 const Menu = (props) => {
     return (
-        <ul className={ styles.Menu }>
-            <li><Button text="przycisk 1" action={() => console.log("przysick")}/></li>
-            <li><Button text="przycisk 2"/></li>
-            <li><Button text="przycisk 3"/></li>
-        </ul>
+        <div className={styles.Menu}>
+            <ul>
+                <li><Button text="przycisk 1" action={() => console.log("przycisk 1")}/></li>
+                <li><Button text="przycisk 2"/></li>
+                <li><Button text="przycisk 3"/></li>
+            </ul>
+        </div>
     )
 }
 

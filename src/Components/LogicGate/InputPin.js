e0c6e8784c613225e7095d1781c02aa4dc6c40d5
isayni <lennysep@protonmail.com>
2022-01-14 21:53:45 +0100

    algorytm DFS

--- src/Components/LogicGate/InputPin.js
+++ src/Components/LogicGate/InputPin.js
@@ -1,5 +1,73 @@
 import Pin from "./Pin";
+import EndNode from "../Node/EndNode";
 
+function checkForCycle(gate) {
+    // algorytm DFS
+    // https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph
+    const gates = collectGates(gate);
+    let visited  = new Array(gates.length);
+    let recStack = new Array(gates.length);
+    for(let i=0;i<gates.length;i++) {
+        visited[i]  = false;
+        recStack[i] = false;
+    }
+
+    for (let i = 0; i < gates.length; i++)
+        if (DFSutil(i, visited, recStack, getChildGates(gates[i]))){
+            return true;
+        }
+    return false;
+}
+
+function DFSutil(i, visited, recStack, children) {
+// Mark the current node as visited and
+    // part of recursion stack
+    if (recStack[i])
+        return true;
+
+    if (visited[i])
+        return false;
+
+    visited[i] = true;
+    recStack[i] = true;
+
+    for (let c=0;c< children.length;c++)
+        if (DFSutil(children, visited, recStack, children)){
+            return true;
+        }
+
+    recStack[i] = false;
+
+    return false;
+}
+function collectGates(gate) {
+    let gates = [gate];
+    const loop = (g) => {
+        getChildGates(g).forEach((child) => {
+            // bramka nie ma jeszcze na liście
+            if(!gates.includes(child)) {
+                gates.push(child);
+                loop(child);
+            }
+        });
+    }
+    loop(gate);
+
+    return gates;
+}
+function getChildGates(gate) {
+    let childGates = [];
+    for (let i = 0; i < gate.outputs.length; i++) {
+        // dodaje wszystkie piny, ktore pobieraja sygnal z aktualnej bramki
+        const childPins = gate.outputs[i].state.childPins;
+        for (let j = 0; j < childPins.length; j++){
+            // pomijamy EndNody
+            if (childPins[j] instanceof EndNode) continue;
+            childGates.push( childPins[j].gate );
+        }
+    }
+    return childGates;
+}
 class InputPin extends Pin {
     constructor(props) {
         super(props);
@@ -34,7 +102,7 @@ class InputPin extends Pin {
         this.setState({'value': signal}, function() {
             // zmieniamy parent pin, wiec sprawdzamy czy wystepuje rekurencja
             if (this.gate.state.recursion) return;
-            if (this.searchForRecursion()){
+            if (checkForCycle(this.gate)){
                 this.gate.setState({"recursion": true},
                     () => setTimeout(
                         () => { this.gate.setState({"recursion": false})}, 500)
--- src/Components/LogicGate/Pin.js
+++ src/Components/LogicGate/Pin.js
@@ -2,18 +2,6 @@ import React from "react";
 import EndNode from "../Node/EndNode";
 import styles from "./LogicGate.module.scss";
 
-// dodaje do tablicy wszystkie bramki pobierajace sygnal z podanej bramki
-function collectChildGates ( childGates, gate ) {
-    for (let i = 0; i < gate.outputs.length; i++){
-        // dodaje wszystkie piny, ktore pobieraja sygnal z aktualnej bramki
-        const childPins = gate.outputs[i].state.childPins;
-        for (let j = 0; j < childPins.length; j++){
-            // pomijamy EndNody
-            if (childPins[j] instanceof EndNode) continue;
-            childGates.push( childPins[j].gate );
-        }
-    }
-}
 class Pin extends React.Component {
     style = styles;
 
@@ -22,23 +10,6 @@ class Pin extends React.Component {
         this.index = props.index;
         this.gate = props.gate
         props.mount(this); // dodaj siebie do tablicy pinów swojej bramki
-    }
-
-    searchForRecursion() {
-        // this = pin typu input
-        // bramka ktorej szukamy (sprawdzamy, czy sie powtarza)
-        const searchedGate = this.gate;
-        const gates = []; // tablica, w ktorej przechowujemy wszystkie bramki do sprawdzenia
-        collectChildGates (gates, searchedGate);
-
-        // dopoki sa jakies bramki do sprawdzenia
-        while ( gates.length !== 0 ){
-            const currentGate = gates.pop();
-            if ( currentGate === searchedGate ) return true;
-            // dodaje wszystkie bramki, ktore pobieraja sygnal z aktualnej bramki
-            collectChildGates ( gates, currentGate );
-        }
-        return false;
     }
 }
 

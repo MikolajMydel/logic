f609be47dab754171e309af960c75877d226dae3
isayni <lennysep@protonmail.com>
2022-01-23 17:57:17 +0100

    funkcje logiczne zwracają tablice outputów

--- src/Components/LogicGate/LogicalFunctions.js
+++ src/Components/LogicGate/LogicalFunctions.js
@@ -1,30 +1,21 @@
-// sprawdzam czy byl undefined dopiero na koncu funkcji
-// aby miec pewnosc, ze nie ma na nastepnych miejscach
-// znaczacych wartosci
-export function OR ( inputs ){
-
-    let isUndefined = false;
-
-    for (let i = 0; i < inputs.length; i++){
-        if ( inputs[i] === true) return true;
-        if (inputs[i] === undefined) isUndefined = true;
-    }
-
-    if ( isUndefined ) return undefined;
-    else return false;
-}
-
-// jezeli jest chociaz jeden false - zwroc false
 // jezeli jest chociaz jeden undefined - zwroc undefined
-export function AND ( inputs ){
-
+export const AND = (inputs) => {
     let isUndefined = false;
-
     for (let i = 0; i < inputs.length; i++){
         if (inputs[i] === false) return false;
         if (inputs[i] === undefined) isUndefined = true;
     }
-
     if ( isUndefined ) return undefined;
-    else return true;
+    return [true];
+}
+
+// jezeli jest chociaz jeden true - zwroc true
+export const OR = (inputs) => {
+    let isUndefined = false;
+    for (let i = 0; i < inputs.length; i++){
+        if ( inputs[i] === true) return true;
+        if (inputs[i] === undefined) isUndefined = true;
+    }
+    if ( isUndefined ) return undefined;
+    return [false];
 }

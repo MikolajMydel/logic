38a80ae4b13a673898263c584c514252a436d518
isayni <lennysep@protonmail.com>
2022-01-27 01:48:54 +0100

    niepotrzebny import

--- src/Components/Application/Application.js
+++ src/Components/Application/Application.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import styles from './Application.module.scss';
 import LogicGate from "../LogicGate/LogicGate";
 import StartNode from "../Node/StartNode";
